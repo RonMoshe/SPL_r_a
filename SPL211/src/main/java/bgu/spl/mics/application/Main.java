7418decdf0dc6e959a100814ddecb44e20398e95
spl211 <moshr@post.bgu.ac.il>
2020-12-06 19:27:15 +0200

    Gson

--- SPL211/src/main/java/bgu/spl/mics/application/Main.java
+++ SPL211/src/main/java/bgu/spl/mics/application/Main.java
@@ -1,9 +1,14 @@
 package java.bgu.spl.mics.application;
 
+import java.bgu.spl.mics.application.messages.AttackEvent;
+import java.bgu.spl.mics.application.passiveObjects.Attack;
+import java.bgu.spl.mics.application.passiveObjects.Ewoks;
 import java.bgu.spl.mics.application.passiveObjects.Input;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
+import java.bgu.spl.mics.application.services.LandoMicroservice;
+import java.bgu.spl.mics.application.services.R2D2Microservice;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -18,7 +23,13 @@ public class Main {
 		}
 		Gson gson = new Gson();
 		try(JsonReader reader = new JsonReader(new FileReader(args[0]))){
-			Input inp = gson.fromJson(reader, Input.class);
+			Input json = gson.fromJson(reader, Input.class);
+			Ewoks ewoks = Ewoks.getInstance(json.getEwoks());
+			LandoMicroservice landoMS = new LandoMicroservice(json.getLando());
+			R2D2Microservice R2D2MS = new R2D2Microservice(json.getR2D2());
+			Attack[] attacks = json.getAttacks();
+			System.out.println("read");
+			//Input json = Input.
 
 		} catch (IOException e) {
 			e.printStackTrace();
